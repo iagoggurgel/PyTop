3608746e8c98ffbf6954e8979892b2c09358515d
Iago Gouveia Gurgel <iagoggurgel25@gmail.com>
2024-08-11 15:30:34 -0300

    FEAT: ProcessSensor ready!

--- src/process.py
+++ src/process.py
@@ -1,71 +1,107 @@
 from dataclasses import dataclass
-from os import scandir
+from os import scandir, DirEntry
 from json import dumps
 from .config import ENV
+from .defaults import Constants
+from time import sleep
 
 class ProcessSensor():
-    
+
     def __init__(self) -> None:
-        self.processes: list[Process] = self.getAllProcesses()
+        self.processList: list[Process] = None
 
-    def getAllProcesses(self):
-        processList: list[Process] = list()
-        with scandir(ENV.get('PROC_PATH')) as procDir:
-            for process in procDir:
-                thisProcess = Process()
-                if process.is_dir() and process.name.isnumeric():
-                    with open(f'{process.path}/status') as statusFile:
-                        for line in statusFile.readlines():
-                            splitted = line.split("\t")
-                            match splitted[0]:
-                                case 'Name:':
-                                    thisProcess.name = splitted[1]
-                                case 'State:':
-                                    thisProcess.status = splitted[1]
-                                case 'Pid:':
-                                    thisProcess.pid = splitted[1]
-                                case 'VmSize:':
-                                    thisProcess.memSize = splitted[1]
-                                case 'Threads:':
-                                    thisProcess.threads = int(splitted[1])
-                if thisProcess.processIsEmpty:
-                    continue
-                processList.append(thisProcess)
-        
-        return processList
-
-    def toJson(self):
+    def scanProcesses(self) -> None:
+        try:
+            while True:
+                processList: list[Process] = list()
+                with scandir(ENV.get('PROC_PATH')) as procDir:
+                    for process in procDir:
+                        if process.is_dir() and process.name.isnumeric():
+                            newProcess = Process(
+                                DIR=process
+                            )
+                            if newProcess.processIsEmpty:
+                                continue
+                            processList.append(newProcess)
+                self.processList = processList
+        except:
+            pass
+            sleep(5)
+    
+    def toJson(self) -> str:
         _ = list()
-        for process in self.processes:
+        for process in self.processList:
             _.append(process.toDict())
-        return dumps(_, indent=4)
+        return dumps(
+            obj=_,
+            indent=4,
+        )
 
-@dataclass(init=False)
+@dataclass()
 class Process():
     """
-    Classe que armazena as informações pertinentes de um processo.
+    Class for storing Process Information.
     """
-    name: str = ''
-    status: str = ''
     pid: str = ''
+    name: str = ''
+    state: str = ''
     memSize: str = ''
+    commandLine: str = ''
     threads: int = 0
-    elapsed: int = 0
+    elapsed: float = 0
+
+    def __init__(self, DIR: DirEntry) -> None:
+        self.DIR: DirEntry = DIR
+        self.setProcessInfo()
+
+    def setProcessInfo(self) -> None:
+        """
+        Opens Status / Sched / Cmdline files and sets the Process Information.
+        """
+        def parseLine(line: str):
+            return tuple(line.replace('\t', '').split(':'))
+
+        with open(f'{self.DIR.path}/{Constants.STATUS}') as FILE:
+            for line in FILE.readlines():
+                parsedLine = parseLine(line)
+                match parsedLine[Constants.KEY]:
+                    case Constants.PID:
+                        self.pid = parsedLine[Constants.VALUE]
+                    case Constants.NAME:
+                        self.name = parsedLine[Constants.VALUE]
+                    case Constants.STATE:
+                        self.state = parsedLine[Constants.VALUE]
+                    case Constants.MEMSIZE:
+                        self.memSize = parsedLine[Constants.VALUE]
+                    case Constants.THREADS:
+                        self.threads = int(parsedLine[Constants.VALUE])
+                        break
+
+        with open(f'{self.DIR.path}/{Constants.SCHED}') as FILE:
+            for line in FILE.readlines():
+                parsedLine = parseLine(line)
+                if Constants.ELAPSED in parsedLine[Constants.KEY]:
+                    self.elapsed = float(parsedLine[Constants.VALUE])
+
+
+        with open(f'{self.DIR.path}/{Constants.CMDLINE}') as FILE:
+            self.commandLine = FILE.read()
 
     @property
-    def processIsEmpty(self):
-        return self.name == '' and self.status == '' and self.pid == '' and self.memSize == '' and self.threads == 0 and self.elapsed == 0
+    def processIsEmpty(self) -> bool:
+        return self.name == '' and self.state == '' and self.pid == '' and self.memSize == '' and self.commandLine == '' and self.threads == 0 and self.elapsed == 0
     
-    def toDict(self):
+    def toDict(self) -> dict:
         return {
-            "name": self.name,
-            "status": self.status,
             "pid": self.pid,
+            "name": self.name,
+            "status": self.state,
             "memSize": self.memSize,
             "threads": self.threads,
-            "elapsed": self.elapsed
+            "elapsed": self.elapsed,
+            "commandLine": self.commandLine,
         }
 
 if __name__ == '__main__':
-    processSensor = ProcessSensor()
-    processSensor.toJson()
+    sensor = ProcessSensor()
+    print(sensor.toJson())
